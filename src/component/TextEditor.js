bd12c5a4d7db34da8629a54914230acf251ddb88
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2022-04-11 10:07:00 -0700

    switched to functional component using hooks

--- src/component/TextEditor.js
+++ src/component/TextEditor.js
@@ -1,39 +1,27 @@
-import React, { Component } from 'react'
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, convertToRaw } from 'draft-js'
-import draftToHtml from 'draftjs-to-html';
+import React, { useState } from "react"
+import { Editor } from "react-draft-wysiwyg"
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+import { EditorState, convertToRaw } from "draft-js"
+import draftToHtml from "draftjs-to-html"
 
-export default class TextEditor extends Component {
-
-    state = {
-        editorState: EditorState.createEmpty()
-    }
-
-    onEditorStateChange = (editorState) => {
-        this.setState({
-            editorState,
-        })
-    }
-
-    render() {
-        const { editorState } = this.state;
-        console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))
-        return (
-            <div>
-                <Editor
-                editorState={editorState}
-                toolbarClassName="toolbarClassName"
-                wrapperClassName="wrapperClassName"
-                editorClassName="editorClassName"
-                onEditorStateChange={this.onEditorStateChange}
-                />
-                <textarea
-                    disabled
-                    value={
-                        draftToHtml(convertToRaw(editorState.getCurrentContent()))
-                }></textarea>
-            </div>
-        )
-    }
+const DraftJs = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty())
+  return (
+    <div>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={setEditorState}
+      />
+        <textarea
+            disabled
+            value={
+                draftToHtml(convertToRaw(editorState.getCurrentContent()))
+        }></textarea>
+    </div>
+  )
 }
+
+export default DraftJs
